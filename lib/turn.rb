3e4b1dce20cfb788ec30be5b1fc960637572a137
Christopher Benedict <chrisjbenedict@gmail.com>
2018-05-02 22:42:39 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,4 +5,7 @@ def display_board(board)
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
-board = [" "," "," "," "," "," "," "," "," "]
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
